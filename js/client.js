8e74a7f65454604e10ecdbb05fc9914f9f6a23cb
SanzayKdg <sanjaykhadgi9861@gmail.com>
2022-10-01 23:57:33 +0545

    added comments
    added comments

--- js/client.js
+++ js/client.js
@@ -1,13 +1,16 @@
 const socket = io('http://localhost:8000');
 
+// Get DOM elements in a respective JS variables 
 const form = document.getElementById('send-container');
 
 const messageInp = document.getElementById('messageInp');
 
 const messageContainer = document.querySelector('.container');
 
+//  Audio will play on message recieve
 var audio = new Audio('notification.mp3');
 
+// function which will append event info to the container 
 const append = (message, position)=>{
     const messageElement  = document.createElement('div');
     messageElement.innerText = message;
@@ -20,23 +23,31 @@ const append = (message, position)=>{
     }
 }
 
+// Ask new user for his/her name and let the server know
+const name = prompt("Enter your name to join the chat");
+
+socket.emit('new-user-joined', name);
+
+// if a new user joins receive his/her name from the server
+socket.on('user-joined', name=>{
+    append(`${name} joined the chat`, 'right')
+})
+
+// if server sends a message receive it
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+// if a user leaves then append the info to the container
+socket.on('left', name=>{
+    append(`${name} left the chat `, 'left')
+})
+
+// if the form get subitted  send server the message
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInp.value;
     append(`You: ${message}` , 'right');
     socket.emit('send', message);
     messageInp.value = "";
-})
-const name = prompt("Enter your name to join the chat");
-
-socket.emit('new-user-joined', name);
-
-socket.on('user-joined', name=>{
-    append(`${name} joined the chat`, 'right')
-})
-socket.on('receive', data=>{
-    append(`${data.name}: ${data.message}`, 'left')
-})
-socket.on('left', name=>{
-    append(`${name} left the chat `, 'left')
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -8,15 +8,18 @@ const io = require('socket.io')(8000, {
 const users = {};
 
 io.on('connection', socket =>{
+    //  if any new users joins, let other connected users notify
     socket.on('new-user-joined', name =>{
-     
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name)
     });
 
+    // if someone sends message broadcast it to all connected users
     socket.on('send', message=>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
     });
+
+    // if any connected user disconnects/lefts the chat lets other connected users know
     socket.on('disconnect', message=>{
         socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
